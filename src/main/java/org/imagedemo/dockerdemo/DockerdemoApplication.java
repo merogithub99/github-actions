f823f5606768f53614fdbb3042782d2e3e00a95d
sushant <sushantbastakoti999@gmail.com>
2025-03-03 17:17:55 +0545

    code

---
+++ src/main/java/org/imagedemo/dockerdemo/DockerdemoApplication.java
@@ -0,0 +1,35 @@
+package org.imagedemo.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class DockerdemoApplication {
+
+
+	@GetMapping
+	public  String welcome(){
+		return "welcome to the docker";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemoApplication.class, args);
+	}
+
+//	…or create a new repository on the command line
+//	echo "# github-actions" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/merogithub99/github-actions.git
+//	git push -u origin main
+//…or push an existing repository from the command line
+//	git remote add origin https://github.com/merogithub99/github-actions.git
+//	git branch -M main
+//	git push -u origin main
+
+}
---
+++ src/test/java/org/imagedemo/dockerdemo/DockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imagedemo.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
